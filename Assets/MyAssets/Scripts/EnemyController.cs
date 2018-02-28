27f7b9030d381070c59b353b30f86b7a72b86e9c
DESKTOP-7C6A92V\Dkill <sorax2204@hotmail.com>
2018-02-28 13:18:39 +0000

    updated level

---
+++ Assets/MyAssets/Scripts/EnemyController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class EnemyController : MonoBehaviour
+{
+
+    public Transform Player;
+    public Transform OriginalPosition;
+    public float MoveSpeed = 2.5f;
+    public float MaxDist = 10;
+    public float MinDist = 1f;
+    public bool isWalking = false;
+    public bool isIdling = true;
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        transform.LookAt(Player);
+
+        if (Vector3.Distance(transform.position, Player.position) <= MinDist)
+        {
+            //Kill Player
+        }
+        else if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+        {
+            if (Vector3.Distance(transform.position, Player.position) >= MinDist) {
+                transform.LookAt(Player);
+                transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+                if (!isWalking){
+                    animator.Play("Idle");
+                    isWalking = true;
+                    isIdling = false;
+                }
+            } 
+        }
+        else {
+            if (Vector3.Distance(transform.position, OriginalPosition.position) >= MinDist)
+            {
+                transform.LookAt(OriginalPosition);
+                transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+                if (!isWalking){
+                    animator.Play("Idle");
+                    isWalking = true;
+                    isIdling = false;
+                }
+            }
+        }
+        if (!isIdling) {
+            animator.Play("Idle");
+            isIdling = true;
+            isWalking = false;
+        }
+    }
+}
--- Assets/MyAssets/Scripts/SkyboxRotator.cs
+++ Assets/MyAssets/Scripts/SkyboxRotator.cs
@@ -3,15 +3,10 @@
 public class SkyboxRotator : MonoBehaviour
 {
     public float RotationPerSecond = 1;
-    private bool _rotate;
+    private bool _rotate = true;
 
     protected void Update()
     {
         if (_rotate) RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotationPerSecond);
-    }
-
-    public void ToggleSkyboxRotation()
-    {
-        _rotate = !_rotate;
     }
 }
