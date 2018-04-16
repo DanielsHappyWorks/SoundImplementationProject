1888470eccfcdd2b1c6cc9abb7f9ce30011f69a6
DESKTOP-7C6A92V\Dkill <sorax2204@hotmail.com>
2018-04-16 17:12:54 +0100

    Added farm animal noises

---
+++ Assets/AnimalNoiseBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalNoiseBehaviour : MonoBehaviour {
+    public AudioClip[] noises;
+    private AudioSource audioSource;
+    public float minTime, maxTime;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        Invoke("PlayNoise", Random.Range(minTime, maxTime));
+    }
+
+    void PlayNoise()
+    {
+        audioSource.clip = noises[(int)Random.Range(0, noises.Length)];
+        audioSource.pitch = (Random.Range(0.9f, 1.1f));
+        audioSource.volume = (Random.Range(0.2f, 0.9f));
+        audioSource.Play();
+        Invoke("PlayNoise", Random.Range(minTime, maxTime));
+    }
+}
