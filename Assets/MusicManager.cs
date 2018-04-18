750f669c895f2cfcb2087dccf165bdc07c296560
DESKTOP-JAG7FVD\GrapeCauliflower <daniel.foth@hotmail.com>
2018-04-18 15:47:16 +0100

    Added background music, updated mixer and animal delays, changed temple positions and filled chest

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+    public AudioClip currentClip;
+    public AudioClip nextClip;
+    public AudioClip defaultClip;
+
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = currentClip;
+        audioSource.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(nextClip.name != currentClip.name)
+        {
+            currentClip = nextClip;
+            audioSource.clip = currentClip;
+        }
+
+        if (!audioSource.isPlaying) {
+            audioSource.Play();
+        }
+	}
+
+    public void SetNextClip(AudioClip ac)
+    {
+        nextClip = ac;
+    }
+
+    public void SetDefault(AudioClip ac)
+    {
+        if (ac.name == currentClip.name)
+        {
+            nextClip = defaultClip;
+        }
+    }
+}
---
+++ Assets/MusicVolumeBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicVolumeBehaviour : MonoBehaviour {
+    public AudioClip volumeClip;
+    MusicManager musicManager;
+
+    // Use this for initialization
+    void Start () {
+        musicManager = GameObject.FindGameObjectWithTag("MusicManager").GetComponent<MusicManager>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            musicManager.SetNextClip(volumeClip);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            musicManager.SetDefault(volumeClip);
+        }
+    }
+}
